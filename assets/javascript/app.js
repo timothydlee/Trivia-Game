c97ff66e258b9e84b2dfe7b9df468cd0de1ea710
timothydlee <timothy.d.lee@live.com>
2016-11-11 16:23:28 -0600

    got a working model of the game, except for the moving on to the next question on each click

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,34 +1,32 @@
-console.log("Is this working")
 	/*Global Variables*/
 	var correct = 0;
 	var incorrect = 0;
-	var timerQ = 10;
-	var timerA = 3;
-	var counter = 0;
-	var rightOrWrong = true;
-	var gameCounter = -1;
-	var answeredYet = true;
-	var flag;
+	var timerQ = 10; //For timer that decrements every second, visually for user
+	var timerA = 3; //For setTimeout
+	var gameCounter = 0; //For keeping track of which question user sees
+	var counter; //For set interval
+	var rightOrWrong;
+	var answeredYet = false;
 
-	/*Object that contains questions/answers*/
+	//Object that contains questions/answers*//
 	var questions = [
 		{
-			"answer": "3",
+			"answer": "Christopher Walken",
 			"question": "Which multiple-time host had the famous need for more cowbell?",
 			"options": ["Alec Baldwin", "Steve Martin", "Tom Hanks","Christopher Walken"]
 		},
 		{
 			"answer": "Kate McKinnon",
-			"question": "Which cast member received fame for her impression of Justin Bieber and Hillary Clinton?",
+			"question": "Which cast member received fame for her impressions of Justin Bieber and Hillary Clinton?",
 			"options": ["Tina Fey", "Kate McKinnon", "Vanessa Bayer", "Amy Poehler", ]
 		},
 		{
 			"answer": "Will Ferrell and Rachel Dratch",
-			"question": "Which duo played the overly sexually married lovers, Roger and Virginia Klarvin? Hint: their most famous scene was in a hot tub",
+			"question": "Which duo played the overly sexually married lovers, Roger and Virginia Klarvin? Hint: Their most famous scene was in a hot tub",
 			"options": ["Will Ferrell and Rachel Dratch", "Fred Armisen and Tina Fey", "Darrell Hammond and Amy Poelher", "Jason Sudeikis and Abby Elliott"]
 		},
 		{
-			"answer": "Darrell  Hammond",
+			"answer": "Darrell Hammond",
 			"question": "Which cast member played Sean Connery as Will Ferrell's nemesis in Celebrity Jeopardy?",
 			"options": ["Jimmy Fallon", "Jim Breuer", "David Spade", "Darrell Hammond"]
 		},
@@ -43,7 +41,7 @@ console.log("Is this working")
 			"options": ["Tigers", "Warriors", "Spartans", "Deers"]
 		},
 		{
-			"answer": "In a van, down by the river",
+			"answer": "In a van down by the river",
 			"question": "Where does Chris Farley's character, Matt Foley, the motivational speaker, live?",
 			"options": ["200 miles away", "Down the street", "1601 Pennsylvania Avenue", "In a van down by the river"]
 		},
@@ -54,35 +52,80 @@ console.log("Is this working")
 		}
 	];
 
-	//TIMER STUFF
-	function runQ(){
-		counter = setInterval(decrementQ, 1000);
-    };
+	$("#start-button").on("click", function(){
+		$(this).addClass("hide");
+		$(".answers").removeClass("hide");
+		$(".question").removeClass("hide");
+		myTimer();
+		game();
+	});
 
-    function decrementQ(){
-        timerQ--;
-        $('#timer').html('<h2>' + timerQ + '</h2>');
-        if (timerQ === 0){
-            stop();
-        }
-    };
+	var test = function(){
+		console.log("Test");
+	}
 
-    function runA(){
-		counter = setInterval(decrementA, 1000);
-    };
+	var game = function(){
+		console.log(gameCounter);
+		if (gameCounter===questions.length){
+			displayScore();
+			return false;
+		} else {
+			displayQuestion();
+			console.log("After set interval");
+		}
+	}
 
-    function decrementA(){
-        timerA--;
-        $("#timer").html("");
-        if (timerA === 0){
-            stop();
-        }
-    };
+	var displayQuestion = function(){
+		console.log("displayQuestion: " + questions[gameCounter].question);
+		$(".question").html(questions[gameCounter].question);
+		for (var i=0; i<questions[gameCounter].options.length; i++){
+			$("#answer" + i).attr("data-name", questions[gameCounter].options[i]).html(questions[gameCounter].options[i]);
+		}
+		counter = setInterval(myTimer, 1000);
+	}
 
+	var displayAnswer = function(){
+		console.log("displayAnswer: " + questions[gameCounter].options[questions[gameCounter].answer]);
+		rightOrWrongDisplay();
+		$(".question").html("The correct answer is " + questions[gameCounter].answer + "!");
+		$(".answers").empty();
+
+	}
+
+	var displayScore = function(){
+		console.log("We made it to the end!");
+		$(".question").html("Game Over! You got " + correct + " questions correct!");;
+		$(".answers").empty();
+	}
+
+
+	function myTimer() {
+		$("#timer").html(timerQ--);
+		if (timerQ===-1){
+			stop();
+			displayAnswer();
+			gameCounter++;
+			setTimeout(game, timerA*1000);
+		}	
+	 };
+	  
     function stop(){
     	clearInterval(counter);
+    	timerQ=10;
     };
-	
+
+    $(".answers").on("click", function(){
+    	var chosenAnswer = ($(this).attr("data-name")); 
+    	if (chosenAnswer === questions[gameCounter].answer){
+    		rightOrWrong = true;
+    		correct++;
+    	} else {
+    		rightOrWrong = false;
+    		incorrect++;
+    	}
+    	answeredYet = true;
+    });
+
     function rightOrWrongDisplay(){
     	if (rightOrWrong===true){
     		$("#timer").html("You are correct!");
@@ -90,83 +133,3 @@ console.log("Is this working")
     		$("#timer").html("You are wrong, dummy!");
     	}
     };
-
-    console.log(questions[counter].options[questions[counter].answer]);
-
-	$("#start-button").on("click", function(){
-		$(this).addClass("hide");
-		$(".answers").removeClass("hide");
-		$(".question").removeClass("hide");
-		game();
-	});
-
-	var game = function(){
-		gameCounter++
-		// if (gameCounter + 1> questions.length){
-		// 	displayScore();
-		// } else {
-		// 	displayQuestion(counter);
-		// 	displayAnswer(counter);
-		// }
-		// gameCounter++;
-		// game();
-	}
-
-	var displayScore = function(){
-		console.log("placeholding");
-	}
-
-	var displayQuestion = function(counter){
-		$(".question").html(questions[counter].question);
-		$("#answer0").html(questions[counter].options[0]);
-		$("#answer1").html(questions[counter].options[1]);
-		$("#answer2").html(questions[counter].options[2]);
-		$("#answer3").html(questions[counter].options[3]);
-	}
-
-	var displayAnswer = function(counter){
-		runA();
-		var questionsArray = questions[counter].options;
-		$("#timer").html(rightOrWrong);
-		if (timer===0){
-			$("#question").html("The correct answer is " + questions[counter].options[questions[counter].answer] + "!");
-		} else {
-			return;
-		}
-	}
-
-
-
-
-	// var displayQuestion = function(x){
-	// 	runQ();
-	// 	console.log("Before the while");
-	// 	if (timerQ !== 0 || answeredYet !== true) {
-	// 		console.log("after the while");
-	// 		$(".question").html(questions[x].question);
-	// 		$("#answer0").html(questions[x].options[0]);
-	// 		$("#answer1").html(questions[x].options[1]);
-	// 		$("#answer2").html(questions[x].options[2]);
-	// 		$("#answer3").html(questions[x].options[3]);
-	// 	} else if (timerQ === 0 || answeredYet === true){
-	// 		displayAnswer(x);
-	// 	}
-	// };
-
-	// var displayAnswer = function(x){
-	// 	run(timerA);
-	// 	while (timerA>0){
-	// 		$("#timer").html(questions[x].answer);
-	// 		$(".question").html("Right");
-	// 		$(".answers").html("");
-	// 		answeredYet = true;
-	// 	}
-	// };
-
-	// var displayEnd = function(){
-	// 	console.log("show results")
-	// };
-
-	// $(".answers").on("click", function(){
-	// 	answeredYet=true;
-	// });
